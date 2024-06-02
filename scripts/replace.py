e60293d6b7f1454578854594f79d33926cc05490
zipuvxiao <38249355+zipuvxiao@users.noreply.github.com>
2024-06-02 21:52:54 +0800

    Create replace.py

---
+++ scripts/replace.py
@@ -0,0 +1,24 @@
+import shutil
+
+def replace_in_file(file_path, old_string, new_string):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    
+    content = content.replace(old_string, new_string)
+    
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+if __name__ == "__main__":
+    # 原文件路径
+    original_file_path = 'IPTV-Unicom.m3u'
+    
+    # 创建并修改 IPTV-UDPXY.m3u
+    new_file_path_udpxy = 'IPTV-UDPXY.m3u'
+    shutil.copyfile(original_file_path, new_file_path_udpxy)
+    replace_in_file(new_file_path_udpxy, '192.168.123.1:23234', 'udpxy.iptv.ikunchina.com:2112')
+    
+    # 创建并修改 IPTV-MSDLITE.m3u
+    new_file_path_msdlite = 'IPTV-MSDLITE.m3u'
+    shutil.copyfile(original_file_path, new_file_path_msdlite)
+    replace_in_file(new_file_path_msdlite, '192.168.123.1:23234', 'msdlite.iptv.ikunchina.com:7088')
